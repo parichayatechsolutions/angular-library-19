93064823e0c86db0196f1417d56d7d60264ce41a
parichayatechsolutions <parichayatechsolutions@gmail.com>
2025-05-20 00:33:11 +0530

    next

--- angular-wc-workspace/projects/wc-consumer-web/src/app/app.component.ts
+++ angular-wc-workspace/projects/wc-consumer-web/src/app/app.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { HelloWorldComponent } from 'shared-wc-lib'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   imports: [
     HelloWorldComponent,
     MatSlideToggleModule,
     MatInputModule,
-    MatFormFieldModule, 
+    MatFormFieldModule,
     FormsModule,
     MatRadioModule,
     MatDatepickerModule,
-    MatNativeDateModule, 
-    MatSelectModule
+    MatNativeDateModule,
+    MatSelectModule,
+    NgIf
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'en-US' } // Optional: Set locale
@@ -29,8 +31,31 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class AppComponent {
   title = 'wc-consumer-web';
-  yourAnswer : any;
+  yourAnswer: any;
   termsNumber: any;
-  selectedDate:any;
-  selectedOption : any
+  selectedDate: any;
+  selectedOption: any;
+  firstRowAnswer: string = '';
+  secondRowAnswer: string = '';
+  userComments: string = ''
+
+  section1Answer = '';
+  section1SubAnswer = '';
+  section1Comment = '';
+
+  section2Answer = '';
+  section2SubAnswer = '';
+  section2Comment = '';
+
+  section3Answer = '';
+  section3SubAnswer = '';
+  section3Comment = '';
+
+  section4Answer = '';
+  section4SubAnswer = '';
+  section4Comment = '';
+
+  section5Answer = '';
+  section5SubAnswer = '';
+  section5Comment = '';
 }
